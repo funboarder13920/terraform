6a05cbabbd5020499091ffab8e8ea850d9e0236b
Valentin Berkes <16121857+funboarder13920@users.noreply.github.com>
2020-01-04 15:18:42 +0100

    Update network.tf

--- network.tf
+++ network.tf
@@ -60,36 +60,3 @@ resource "aws_subnet" "public-c" {
     service = "network"
   }
 }
-
-
-# Private subnets in each AZ
-module "subnet_private_a" {
-  source             = "./modules/private_subnet"
-  name               = "private-a"
-  availability_zone  = "eu-west-2a"
-  vpc_id             = aws_vpc.default.id
-  nat_gateway_subnet = aws_subnet.public-a.id
-  cidr_block         = "172.31.48.0/20"
-}
-
-module "subnet_private_b" {
-  source             = "./modules/private_subnet"
-  name               = "private-b"
-  availability_zone  = "eu-west-2b"
-  vpc_id             = aws_vpc.default.id
-  nat_gateway_subnet = aws_subnet.public-b.id
-  cidr_block         = "172.31.64.0/20"
-}
-
-module "subnet_private_c" {
-  source             = "./modules/private_subnet"
-  name               = "private-c"
-  availability_zone  = "eu-west-2c"
-  vpc_id             = aws_vpc.default.id
-  nat_gateway_subnet = aws_subnet.public-c.id
-  cidr_block         = "172.31.80.0/20"
-}
-
-locals {
-  private_subnets = [module.subnet_private_a.id, module.subnet_private_b.id, module.subnet_private_c.id]
-}
